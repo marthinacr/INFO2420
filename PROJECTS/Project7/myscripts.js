d3bd05abd4c30b9a4268583aa25e047b2ecf4c99
Marthina Jensen <10996371@uvu.edu>
2025-04-10 14:26:00 -0600

    Project 7

--- PROJECTS/Project7/myscripts.js
+++ PROJECTS/Project7/myscripts.js
@@ -1,45 +1,52 @@
-let PrintButton1 = new Image();
-	let PrintButton2 = new Image();
-	let logo1 = new Image();
-	let logo2 = new Image();
+PrintButton1 = new Image
+PrintButton2 = new Image
+logo1 = new Image
+logo2 = new Image
+if (document.images) {
+PrintButton1.src = "images/print1.png"
+PrintButton2.src = "images/print2.png"
+logo1.src = "images/logo.jpg"
+logo2.src = "images/logo.png"
+}
 
-	if (document.images) {
-		PrintButton1.src = "images/print1.png";
-		PrintButton2.src = "images/print2.png";
-		logo1.src = "images/logo.jpg";
-		logo2.src = "images/logo.png";
-	}
 
-	const imgArray = [
+	var imgArray = new Array(
 		'DogTrick2Lg.jpg',
 		'HuntingDogsLg.jpg',
 		'KidsDogLg.jpg',
 		'Pet1Lg.jpg'
-	];
+	);
 
-	const titleArray = [
-		'Dog Performing a Trick',
-		'Two Dogs',
-		'Dog and Boys Portrait',
+
+	var titleArray = new Array(
+		'Dog doing a Trick',
+		'Two Hunting Dogs',
+		'Kids and Dog Portrait',
 		'Women and Dog'
-	];
-
-	const imgPath = "images/Pet/";
-
+		
+	);
+		
+	var imgPath = "images/Pet/";
+	
 	function swapImage(imgID) {
-		const theImage = document.getElementById('theImage');
-		const textDiv = document.getElementById('bottomText');
 
-		theImage.src = imgPath + imgArray[imgID];
-		textDiv.innerHTML = titleArray[imgID];
+		var theImage = document.getElementById('theImage');
+		var textDiv = document.getElementById('bottomText');
+
+		var newImg;
+		var textTitle;
+
+		newImg = imgArray[imgID];
+		theImage.src = imgPath + newImg;
+
+		textTitle=titleArray[imgID];
+
+			  textDiv.innerHTML = textTitle;
 	}
-
-	function preloadImages() {
-		for (let i = 0; i < imgArray.length; i++) {
-			const tmpImg = new Image();
+		
+	function preloadImages() {		
+		for(var i = 0; i < imgArray.length; i++) {
+			var tmpImg = new Image;
 			tmpImg.src = imgPath + imgArray[i];
 		}
 	}
-window.onload = function() {
-	preloadImages();
-};
